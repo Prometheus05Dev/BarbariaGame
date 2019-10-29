a229c4d7a35fe4bb02282baa0e7758b2f749938c
Prometheus05Dev <minecraftjanek05@gmail.com>
2019-10-29 20:40:29 +0100

    Starting to load the game textures, edit relative path! Maybe Game Installation folder?

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/janek/Dokumente/Programmieren/Barbaria
+# Install script for directory: /home/janek/CLionProjects/Barbaria
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/janek/Dokumente/Programmieren/Barbaria/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/janek/CLionProjects/Barbaria/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
