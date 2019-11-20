a1aec7773e83b8a40bccee53a9efa8bbc406fa1c
Prometheus05Dev <minecraftjanek05@gmail.com>
2019-11-20 16:26:22 +0100

    Finished writing the constructors of the Model class.

--- engine/graphics/model.cpp
+++ engine/graphics/model.cpp
@@ -1,2 +1,28 @@
 #include "model.h"
 
+Model::Model(Vertex *vertexData, int numVertices, GLuint *indices, int numIndices) {
+    this->vertexData = vertexData;
+    this->numVertices = numVertices;
+    this->indices = indices;
+    this->numIndices = numIndices;
+}
+
+Model::Model(VertexWT *vertexData, int numVertices, GLuint *indices, int numIndices, const char *texturePath) {
+    this->vertexTData = vertexData;
+    this->numVertices = numVertices;
+    this->indices = indices;
+    this->numIndices = numIndices;
+    this->texturePath = texturePath;
+    texture = true;
+}
+
+Model::Model(VertexWT *vertexData, int numVertices, GLuint *indices, int numIndices, const char **texturePathes,
+             const char *bindOrder) {
+    this->vertexTData = vertexData;
+    this->numVertices = numVertices;
+    this->indices = indices;
+    this->numIndices = numIndices;
+    this->texturePathes = texturePathes;
+    this->bindOrder = bindOrder;
+    texture = true;
+}
--- engine/graphics/model.h
+++ engine/graphics/model.h
@@ -8,6 +8,16 @@ public:
     Model(Vertex *vertexData, int numVertices, GLuint *indices, int numIndices); //Constructor for models WITHOUT texture
     Model(VertexWT *vertexData, int numVertices, GLuint* indices, int numIndices, const char* texturePath); //Constructor for models WITH  ONE texture
     Model(VertexWT *vertexData, int numVertices, GLuint* indices, int numIndices, const char* *texturePathes, const char* bindOrder); //Constructor for models WITH texture
+protected:
+    bool texture = false;
+    Vertex *vertexData;
+    VertexWT *vertexTData;
+    int numVertices;
+    GLuint* indices;
+    int numIndices;
+    const char* texturePath;
+    const char* *texturePathes;
+    const char* bindOrder;
 };
 
 #endif //BARBARIACLION_MODEL_H
