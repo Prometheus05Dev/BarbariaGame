7a4299e6aae0ea03e3b6d29bb9b8aecdb69701a0
Prometheus05Dev <minecraftjanek05@gmail.com>
2020-02-26 09:01:26 +0100

    Final preparations for model loading, everything is now dynamically

--- engine/graphics/object.cpp
+++ engine/graphics/object.cpp
@@ -9,7 +9,7 @@ Object::Object() {
     vertices.push_back(Vertex{1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f});
     vertices.push_back(Vertex{0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f});
     vertices.push_back(Vertex{1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f});
-    
+
     indices.push_back(0);
     indices.push_back(1);
     indices.push_back(3);
